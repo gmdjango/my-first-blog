e829a957239acd0ddb8e908379d78b609a9c057c
George Mogilevsky <g1@methodicalsolutions.com>
2018-07-23 22:39:20 -0400

    fixed issue with blog.py it had queryset error

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from .models import Post
 # Create your views here.
 
 def post_list(request):
-    posts = Post.object.get(published_date__lte=timezone.now()).order_by('-published_date'))
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     # request - this is what we received from the user via the internet
     # 'blog/post_list.html' - this is our template file to renter
     # {} is a dictionary of parameters to use in the template
