02343190b9b7e223e630abd962a5aacc50e3238a
George Mogilevsky <g1@methodicalsolutions.com>
2018-07-23 22:10:33 -0400

    modified Templates to display posts from database and views to pass it variables

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+
 
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.object.get(published_date__lte=timezone.now()).order_by('-published_date'))
+    # request - this is what we received from the user via the internet
+    # 'blog/post_list.html' - this is our template file to renter
+    # {} is a dictionary of parameters to use in the template
+    return render(request, 'blog/post_list.html', {'posts':posts})
